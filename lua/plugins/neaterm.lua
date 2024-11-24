801dfdc22b910f41c43d3f6f70b4a84f0a92ff9f
prodigalbeta <137588668+prodigalbeta@users.noreply.github.com>
2024-11-23 21:19:02 -0800

    batman

---
+++ lua/plugins/neaterm.lua
@@ -0,0 +1,12 @@
+return {
+    "Dan7h3x/neaterm.nvim",
+    branch = "stable",
+    event = "VeryLazy",
+    opts = {
+      -- Your custom options here (optional)
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "ibhagwan/fzf-lua",
+    },
+}
